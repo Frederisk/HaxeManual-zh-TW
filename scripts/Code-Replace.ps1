bf589d006a69d8a0d869448d44405363571a2beb
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2021-12-09 14:13:23 +0800

    Update scripts

--- scripts/Code-Replace.ps1
+++ scripts/Code-Replace.ps1
@@ -1,10 +1,25 @@
 using namespace System;
+using namespace System.Text.RegularExpressions;
 
-#file path
-[String]$file = Read-Host -Prompt "Enter the file name";
+# file path
+[String]$file = Read-Host -Prompt 'Enter the file path';
 $file = $file.Trim().Trim('"');
 
 # exists
 if ((Get-Item -Path $file).Exists) {
+    $dictionary =
+    [String]$dictionary = Read-Host -Prompt 'Enter the source code dictionary path' | Join-Path -Path $dictionary -ChildPath '*';
 
+    [String]$content = Get-Content -Path $file -Raw;
+
+    Get-ChildItem -Path $dictionary -Include '*.hx'
+    | ForEach-Object -Process {
+        [String]$sourceCode = Get-Content -Path $_.FullName -Raw;
+        $content = [Regex]::Replace($content, "(?i)^\[code asset\]\(assets\/$($_.NameString)\)", $sourceCode);
+    }
+    # write
+    Set-Content -Path $file -Value $content;
+}
+else {
+    Write-Error -Message 'File does not exist';
 }
--- scripts/Term-Replace.ps1
+++ scripts/Term-Replace.ps1
@@ -1,36 +1,25 @@
 using namespace System;
 using namespace System.Collections.Generic;
 using namespace System.Linq;
-using namespace System.Text;
 
 # file path
 [String]$file = Read-Host -Prompt "Enter the file path you want to replace term for";
 $file = $file.Trim().Trim('"')
 # exists
 if ((Get-Item -Path $file).Exists) {
-    # create terms list
-    [List[String[]]]$terms = [List[String[]]]::new();
+    [String]$content = Get-Content -Path $file -Raw;
     Get-Content -Path .\Terminology.md
     | Select-String -Pattern '^[\sa-z0-9_-]*?\|' -Raw
     | ForEach-Object -Process {
-        $terms.Add(
-            [Enumerable]::Select(
-                $_.Split('|'),
-                [Func[String, String]] { $args[0].Trim() }
-            )
-        );
-    }
-    # replace
-    [String]$content = Get-Content -Path $file -Raw;
-    # [StringBuilder]$builder = [StringBuilder]::new($content);
-    $terms | ForEach-Object -Process {
-        # [Void]$builder.Replace($_[0], "$($_[0])($($_[1])|$($_[2]))", [StringComparison]::OrdinalIgnoreCase);
-        $content = $content.Replace($_[0], "$($_[0])($($_[1])|$($_[2]))", [StringComparison]::OrdinalIgnoreCase);
+        [IEnumerable[String]]$term = [Enumerable]::Select(
+            $_.Split('|'),
+            [Func[String, String]] { return $args[0].Trim(); }
+        ).ToArray();
+        $content = $content.Replace($term[0], "$($term[0])($($term[1])|$($term[2]))", [StringComparison]::OrdinalIgnoreCase);
     }
     # write
-    # Set-Content -Path $file -Value $builder.ToString();
     Set-Content -Path $file -Value $content;
 }
 else {
-    Write-Host -Object "File does not exist" -ErrorAction;
+    Write-Error -Message "File does not exist";
 }
